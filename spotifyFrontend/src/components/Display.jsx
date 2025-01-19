d497a3bd4959c6b8504aeb0725a0b35a7399c9c8
joksdemic <jovanademic2@gmail.com>
2025-01-19 22:21:07 +0100

    Create the display

---
+++ spotifyFrontend/src/components/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Display = () => {
+  return (
+    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+        
+    </div>
+    
+)
+}
+
+export default Display
