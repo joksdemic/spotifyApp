47ae245ab4fb1d0865c6b5791b572775a5f3e8d6
joksdemic <jovanademic2@gmail.com>
2025-01-20 02:01:36 +0100

    Add time logic

--- spotifyFrontend/src/components/Player.jsx
+++ spotifyFrontend/src/components/Player.jsx
@@ -5,7 +5,7 @@ import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
 
-  const {seekBg,seekBar,playStatus,play,pause, track} = useContext(PlayerContext)
+  const {seekBg,seekBar,playStatus,play,pause,track,time} = useContext(PlayerContext)
 
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
@@ -28,11 +28,11 @@ const Player = () => {
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
-          <p>1:07</p>
+          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
           <div ref={seekBg} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
             <hr ref={seekBar} className="h-1 border-none w-0 bg-green-800 rounded-full" />
           </div>
-          <p>3:20</p>
+          <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
       <div className="hidden lg:flex items-center gap-2 opacity-75">
