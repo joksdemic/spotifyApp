9950cdb15409372fe7409042d64c2a8790edd44f
joksdemic <jovanademic2@gmail.com>
2025-01-20 22:00:32 +0100

    Add albums into database with Cloudinary upload

--- spotifyBackend/server.js
+++ spotifyBackend/server.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import songRouter from './src/routes/songRoute.js';
-import connectDB from './src/config/mongodb.js';
-import connectCloudinary from './src/config/cloudinary.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import songRouter from "./src/routes/songRoute.js";
+import connectDB from "./src/config/mongodb.js";
+import connectCloudinary from "./src/config/cloudinary.js";
+import albumRouter from "./src/routes/albumRoute.js";
 
 //App config
 const app = express();
@@ -16,8 +17,8 @@ app.use(express.json());
 app.use(cors());
 
 //Initializing routes
-app.use('/api/song',songRouter)
+app.use("/api/song", songRouter);
+app.use("/api/album", albumRouter);
+app.get("/", (req, res) => res.send("API Working"));
 
-app.get('/',(req,res)=> res.send("API Working"))
-
-app.listen(port,()=>console.log(`Server started on ${port}`))
+app.listen(port, () => console.log(`Server started on ${port}`));
