20268b3a3f075d0a024c2f3ff2640972efcb56a0
joksdemic <jovanademic2@gmail.com>
2025-01-20 02:04:09 +0100

    Add green bar logic

--- spotifyFrontend/src/context/PlayerContext.jsx
+++ spotifyFrontend/src/context/PlayerContext.jsx
@@ -35,6 +35,7 @@ const PlayerContextProvider = (props) => {
     useEffect(() => {
         setTimeout(() => {
             audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
                 setTime({
                     currentTime: {
                         second: Math.floor(audioRef.current.currentTime % 60),
