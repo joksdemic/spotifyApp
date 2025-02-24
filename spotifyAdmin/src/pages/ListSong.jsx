480b3136b3d086cb07e0affdabb4d6b1606d7a99
joksdemic <jovanademic2@gmail.com>
2025-02-24 18:25:12 +0100

    Add more songs

--- spotifyAdmin/src/pages/ListSong.jsx
+++ spotifyAdmin/src/pages/ListSong.jsx
@@ -63,6 +63,7 @@ const ListSong = () => {
               >
                 x
               </p>
+              
             </div>
           );
         })}
--- spotifyFrontend/src/components/DisplayAlbum.jsx
+++ spotifyFrontend/src/components/DisplayAlbum.jsx
@@ -1,10 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const DisplayAlbum = ({ album }) => {
   const { id } = useParams();
@@ -36,7 +34,7 @@ const DisplayAlbum = ({ album }) => {
               src={assets.spotify_logo}
               alt=""
             />
-            <b className="mr-3"> Spotify</b>■ 1,322,123 likes
+            <b className="mr-3">Spotify</b>■ 1,322,123 likes
             <b className="ml-3">■ 30 songs, </b>
             about 2 hr 15 min
           </p>
@@ -60,12 +58,13 @@ const DisplayAlbum = ({ album }) => {
             className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
           >
             <p className="text-white">
-              <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
-              <img className="inline w-10 mr-5" src={item.image} alt="" />
+              <b className="mr-1 text-[#a7a7a7]">{index + 1}</b>
+              <img className="inline w-10 mr-1" src={item.image} alt="" />
               {item.name}
             </p>
             <p className="text-[15px]">{albumData.name}</p>
             <p className="text-[15px] hidden sm:block">5 days ago</p>
+            {/* Direktno prikazivanje item.duration bez ikakvog formatiranja */}
             <p className="text-[15px] text-center">{item.duration}</p>
           </div>
         ))}
